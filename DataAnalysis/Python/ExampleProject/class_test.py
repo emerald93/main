c3a8efc54192ee7b5efc67c459211b68083798df
emerald93 <ngoc.tb.93@gmail.com>
2019-10-29 13:50:44 -0400

    update python linear model

--- DataAnalysis/Python/ExampleProject/class_test.py
+++ DataAnalysis/Python/ExampleProject/class_test.py
@@ -1,7 +1,7 @@
 # playing with data using Class
 
 import pandas as pd
-from sklearn import datasets, linear_model
+from sklearn import linear_model
 
 # return dependent and independent variables from csv file
 class DataCSV:
@@ -19,6 +19,20 @@ class DataCSV:
         y = [self.data[name_columns[0]]]
         return y
 
+# create a differenced series
+# def difference2(dataset, interval=1):
+#     diff = list();
+#     dataset = np.array(dataset)
+#
+#     for row in range(interval, len(dataset)):
+#         row_data = []
+#         for col in range(int(count(X) / len(X))):
+#             # print(str(row) + ":" + str(col))
+#             value = dataset[row][col] - dataset[row - interval][col]
+#             row_data.append(value)
+#         diff.append(row_data)
+#
+#     return diff
 
 # name_columns = ['date','effr', 'gdp', 'u', 'nber', 'cpi', 'm2', 'oil', 'ip', 'ys', 'cli']
 columns = ['gdp', 'effr', 'u']
@@ -27,4 +41,9 @@ X = data.get_x(columns)
 y = data.get_y(columns)
 
 model = linear_model.LinearRegression()
+
+b0 = model.intercept_
+b1 = model.coef_
+
+print('gdp='+str(b0)+"+"+str(b1)+"effr")
 
--- DataAnalysis/Python/ExampleProject/gdp_effr.py
+++ DataAnalysis/Python/ExampleProject/gdp_effr.py
@@ -1,34 +1,107 @@
 # Q: would lower interest rate improve the economy?
 
 import pandas as pd
+import numpy as np
 import statsmodels.api as sm
+from statsmodels.tsa.stattools import adfuller
+from sklearn import linear_model
 
-data = pd.read_csv('data.csv').dropna()
 
-y = data['gdp']
-X = data[['effr']]
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+data = pd.read_csv('Datasets/data.csv', index_col='date').dropna()
+
+# y = data['gdp']
+# X = data[['effr','gdp']]
+# variables = data[['ys', 'cli', 'gdp', 'cpi', 'm2', 'effr', 'oil', 'u', 'ip']]
+variables = data[['gdp', 'effr']]
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
 #
 # model = linear_model.LinearRegression()
 # reg = model.fit(X_train, y_train)
+# series = read_csv('Datasets/daily-total-female-births.csv', header=0, index_col=0)
+# X = series.values
+# print(X.iloc[:,0])
 
-# b_x =  model.coef_
-# print('Coefficients:', b_x)
-#
-# b0 = model.intercept_
-# print('Intercept: %.2f' % b0)
-#
-# y_pred = model.predict(X_test)
-# var = r2_score(y_test, y_pred)
-# print('Variance score: %.2f' % var)
+
+def is_stationary(X, sig=.05):
+    """
+        Stationary test for multiple variables using Augmented Dickey-Fuller unit root test
+
+        Parameters
+        ----------
+        X: array_like
+            list of variables
+        sig: float
+            significant level
+
+        Returns
+        -------
+        variable is Stationary or Non-stationay
+    """
+    adf_titles = ['tstat', 'pvalue', 'lags', 'nobs', 'critical value', 'icbest']
+    adf_results = {}
+    data_index = 0
+    adf_mes2 = {}
+    # print(X)
+
+    for x in X:
+        adf = adfuller(X.iloc[:, data_index])
+        data_index = data_index + 1
+
+        for i in range(len(adf_titles)):
+            adf_results[adf_titles[i]] = adf[i]
+
+        if adf_results['pvalue'] < sig:
+            adf_mes = x + " is Stationary"
+            adf_mes2[x] = True
+        else:
+            adf_mes = x + " is Non-stationary"
+            adf_mes2[x] = False
+
+        # print(adf_mes)
+
+    return adf_mes2
+
+
+def difference(ds, interval=1):
+    """
+    :param ds: array_like 1d, original data
+    :param interval: int
+    :return: array_like 1d, differencing at interval level data
+    """
+    diff = list()
+    diff.append(None)
+    ds = np.array(ds)
+
+    for i in range(interval, len(ds)):
+        value = ds[i] - ds[i - interval]
+        diff.append(value)
+    return diff
+
+
+stationary_results = is_stationary(variables)
+for sr in stationary_results:
+    if stationary_results[sr]:
+        variables[sr] = variables[sr]
+    else:
+        variables[sr] = difference(variables[sr])
+variables = variables.dropna()
+
+lag = 2
+X = variables['effr'].shift(lag).dropna()
+y = variables['gdp'].tail(-lag)
 
 X2 = sm.add_constant(X)
 est = sm.OLS(y, X2)
 est2 = est.fit()
 print(est2.summary())
 
-# Conclusion:
-# positive coefficient means the economy will go down (gpd decreases) if effr decreases
-# Howeever, effr is not statistically significant at any significant level
-# Therefore, lower interest rate will not improve the economy
+"""
+    Conclusion:
+    -----------
+    positive coefficient means the economy will go down (gpd decreases) if effr decreases
+    effr is statistically significant at any significant level
+    Therefore, lower interest rate will not improve the economy
+
+"""
+
 
